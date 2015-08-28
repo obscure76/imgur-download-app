6885751207bd223d33dc534dcf25eebbc95e5033
obscure76 <midhun.nitw@gmail.com>
2015-08-27 23:48:03 -0700

    second commitss

--- multiprocess-imgur.py
+++ multiprocess-imgur.py
@@ -6,17 +6,16 @@ from time import time
 import os
 
 def main():
-   ts = time()
-   client_id = os.getenv('IMGUR_CLIENT_ID')
-   if not client_id:
-       raise Exception("Couldn't find IMGUR_CLIENT_ID environment variable!")
-   download_dir = setup_download_dir()
-   links = [l for l in get_links(client_id) if l.endswith('.jpg')]
-   download = partial(download_link, download_dir)
-   p = Pool(8)
-   '''with Pool(8) as p:'''
-   p.map(download, links)
-   print('Took {}s'.format(time() - ts))
+    ts = time()
+    client_id = os.getenv('IMGUR_CLIENT_ID')
+    if not client_id:
+        raise Exception("Couldn't find IMGUR_CLIENT_ID environment variable!")
+    download_dir = setup_download_dir()
+    links = [l for l in get_links(client_id) if l.endswith('.jpg')]
+    download = partial(download_link, download_dir)
+    p = Pool(8)
+    p.map(download, links)
+    print('Took {}s'.format(time() - ts))
 
 if __name__ == '__main__':
     main()
